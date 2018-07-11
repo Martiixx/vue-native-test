cdce40395a7bf52be89fc88caf1d534ae6b8e41f
Martiixx_c <martiixx.carrasco@gmail.com>
2018-07-11 13:31:58 -0400

    first commit

---
+++ App.vue
@@ -0,0 +1,38 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary" v-bind:on-press="handleBtnPress">{{text}}</text>
+    <text-input
+        :style="{height: 40, borderColor: 'gray', borderWidth: 1}"
+        v-model="text"
+      />
+    </view>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      model: {
+      },
+      message: "Hello World asdasd asda sdasda",
+      text: ''
+    };
+  },
+  methods: {
+    handleBtnPress: function() {
+      alert('probando cambios' + this.text);
+    }
+  }
+};
+</script>
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
